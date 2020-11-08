e79fbd67e8227cbf9bdc90e04efd5f716b472b64
Nelson Pestana <hello@nelsonpestana.eu>
2020-11-08 00:09:41 +0000

    End of section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
